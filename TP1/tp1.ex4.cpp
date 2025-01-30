3bed3cdf166b569d4a3d66fea82fd8e122eaa3d8
daganaus <swann.daganaud@gmail.com>
2025-01-30 18:36:31 +0100

    changement 30/01

--- TP1/tp1.ex4.cpp
+++ TP1/tp1.ex4.cpp
@@ -5,9 +5,8 @@ using namespace std;
 char tab[20][10];
 int main()
 {
-char *ptr;
-ptr = tab[3];
-;
-cout << "\nvaleur de *ptr " << *ptr << endl;
-return 0;
+    char *ptr;
+    ptr = tab[3];
+    cout << "valeur de *ptr " << (int)*ptr << endl;
+    return 0;
 }
